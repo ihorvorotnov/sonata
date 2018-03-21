90c7012a230dcc958c636970aad75cbddc87b38f
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2018-03-22 00:31:12 +0200

    Re-align some code blocks

--- config/production.php
+++ config/production.php
@@ -5,17 +5,17 @@
  * @package WordPress
  */
 
-define( 'WP_DEBUG',                 false );
-define( 'WP_DEBUG_DISPLAY',         false );
-define( 'WP_DEBUG_LOG',             false );
-define( 'SAVEQUERIES',              false );
-define( 'SCRIPT_DEBUG',             false );
-define( 'WP_ALLOW_REPAIR',          true ); // {$your_site}/wp-admin/maint/repair.php
+define( 'WP_DEBUG',              false );
+define( 'WP_DEBUG_DISPLAY',      false );
+define( 'WP_DEBUG_LOG',          false );
+define( 'SAVEQUERIES',           false );
+define( 'SCRIPT_DEBUG',          false );
+define( 'WP_ALLOW_REPAIR',       true ); // {$your_site}/wp-admin/maint/repair.php
 
-define( 'AUTOSAVE_INTERVAL',        300 );
-define( 'WP_POST_REVISIONS',        false );
-define( 'EMPTY_TRASH_DAYS',         7 ); // Disable trash
+define( 'AUTOSAVE_INTERVAL',     300 );
+define( 'WP_POST_REVISIONS',     false );
+define( 'EMPTY_TRASH_DAYS',      7 ); // Disable trash
 
-define( 'WP_CRON_LOCK_TIMEOUT',     60 );
-define( 'DISABLE_WP_CRON',          false );
-define( 'ALTERNATE_WP_CRON',        false );
+define( 'WP_CRON_LOCK_TIMEOUT',  60 );
+define( 'DISABLE_WP_CRON',       false );
+define( 'ALTERNATE_WP_CRON',     false );
--- wp-config.php
+++ wp-config.php
@@ -50,46 +50,46 @@ require_once ROOTPATH . '/config/' . WP_ENV . '.php';
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv( 'AUTH_KEY' ) );
-define( 'SECURE_AUTH_KEY',  getenv( 'SECURE_AUTH_KEY' ) );
-define( 'LOGGED_IN_KEY',    getenv( 'LOGGED_IN_KEY' ) );
-define( 'NONCE_KEY',        getenv( 'NONCE_KEY' ) );
-define( 'AUTH_SALT',        getenv( 'AUTH_SALT' ) );
-define( 'SECURE_AUTH_SALT', getenv( 'SECURE_AUTH_SALT' ) );
-define( 'LOGGED_IN_SALT',   getenv( 'LOGGED_IN_SALT' ) );
-define( 'NONCE_SALT',       getenv( 'NONCE_SALT' ) );
+define( 'AUTH_KEY',          getenv( 'AUTH_KEY' ) );
+define( 'SECURE_AUTH_KEY',   getenv( 'SECURE_AUTH_KEY' ) );
+define( 'LOGGED_IN_KEY',     getenv( 'LOGGED_IN_KEY' ) );
+define( 'NONCE_KEY',         getenv( 'NONCE_KEY' ) );
+define( 'AUTH_SALT',         getenv( 'AUTH_SALT' ) );
+define( 'SECURE_AUTH_SALT',  getenv( 'SECURE_AUTH_SALT' ) );
+define( 'LOGGED_IN_SALT',    getenv( 'LOGGED_IN_SALT' ) );
+define( 'NONCE_SALT',        getenv( 'NONCE_SALT' ) );
 define( 'WP_CACHE_KEY_SALT', getenv( 'WP_CACHE_KEY_SALT' ) );
 /**#@-*/
 
 /**
  * WordPress Database connection details.
  */
-define( 'DB_NAME',          getenv( 'DB_NAME' ) );
-define( 'DB_USER',          getenv( 'DB_USER' ) );
-define( 'DB_PASSWORD',      getenv( 'DB_PASSWORD' ) );
-define( 'DB_HOST',          getenv( 'DB_HOST' ) );
-define( 'DB_CHARSET',       'utf8' );
-define( 'DB_COLLATE',       '' );
+define( 'DB_NAME',     getenv( 'DB_NAME' ) );
+define( 'DB_USER',     getenv( 'DB_USER' ) );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
+define( 'DB_HOST',     getenv( 'DB_HOST' ) );
+define( 'DB_CHARSET',  'utf8' );
+define( 'DB_COLLATE',  '' );
 
-$table_prefix =             getenv( 'DB_PREFIX' );
+$table_prefix = getenv( 'DB_PREFIX' );
 
 /**
  * Dynamic URLs and paths.
  */
-define( 'WP_HOME',          getenv( 'APP_URL' ) );
-define( 'WP_SITEURL',       getenv( 'APP_URL' ) . '/core' );
+define( 'WP_HOME',    getenv( 'APP_URL' ) );
+define( 'WP_SITEURL', getenv( 'APP_URL' ) . '/core' );
 
-define( 'WP_CONTENT_DIR',   ROOTPATH . '/app' );
-define( 'WP_CONTENT_URL',   getenv( 'APP_URL' ) . '/app' );
+define( 'WP_CONTENT_DIR', ROOTPATH . '/app' );
+define( 'WP_CONTENT_URL', getenv( 'APP_URL' ) . '/app' );
 
 /**
  * Additional configuration constants.
  */
-define( 'AUTOMATIC_UPDATER_DISABLED',   true );
-define( 'DISALLOW_FILE_EDIT',           true );
-define( 'FORCE_SSL_ADMIN',              true );
-define( 'IMAGE_EDIT_OVERWRITE',         true );
-define( 'IMAGE_JPEG_QUALITY',           80 );
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define( 'DISALLOW_FILE_EDIT',         true );
+define( 'FORCE_SSL_ADMIN',            true );
+define( 'IMAGE_EDIT_OVERWRITE',       true );
+define( 'IMAGE_JPEG_QUALITY',         80 );
 
 /**
  * Memcached object cache backend.
